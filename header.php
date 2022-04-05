591e60135bc970e4977cb14e1abb18430ff38eb7
Christofer Stäke <christofer.stake@hiq.se>
2022-04-05 12:42:40 +0200

    Added content-wrapper

--- header.php
+++ header.php
@@ -30,38 +30,40 @@
   <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'philipson' ); ?></a>
 
   <header id="masthead" class="site-header">
-    <div class="site-branding">
-      <?php
-      the_custom_logo();
-      if ( is_front_page() && is_home() ) :
-        ?>
-        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+    <div class="content-wrapper">
+      <div class="site-branding">
         <?php
-      else :
-        ?>
-        <div class="site-title">
-          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-            <?php bloginfo( 'name' ); ?>
-          </a>
-        </div>
-        <?php
-      endif;
-      $philipson_description = get_bloginfo( 'description', 'display' );
-      if ( $philipson_description || is_customize_preview() ) :
-        ?>
-        <p class="site-description"><?php echo $philipson_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-      <?php endif; ?>
-    </div><!-- .site-branding -->
+        the_custom_logo();
+        if ( is_front_page() && is_home() ) :
+          ?>
+          <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+          <?php
+        else :
+          ?>
+          <div class="site-title">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+              <?php bloginfo( 'name' ); ?>
+            </a>
+          </div>
+          <?php
+        endif;
+        $philipson_description = get_bloginfo( 'description', 'display' );
+        if ( $philipson_description || is_customize_preview() ) :
+          ?>
+          <p class="site-description"><?php echo $philipson_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+        <?php endif; ?>
+      </div><!-- .site-branding -->
 
-    <nav id="site-navigation" class="main-navigation">
-      <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'philipson' ); ?></button>
-      <?php
-      wp_nav_menu(
-        array(
-          'theme_location' => 'menu-1',
-          'menu_id'        => 'primary-menu',
-        )
-      );
-      ?>
-    </nav><!-- #site-navigation -->
+      <nav id="site-navigation" class="main-navigation">
+        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'philipson' ); ?></button>
+        <?php
+        wp_nav_menu(
+          array(
+            'theme_location' => 'menu-1',
+            'menu_id'        => 'primary-menu',
+          )
+        );
+        ?>
+      </nav><!-- #site-navigation -->
+    </div>
   </header><!-- #masthead -->
